b9bb509927abb0b12293fd5ed4ddb00874bec89d
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-17 22:45:35 +0530

    jquery4

---
+++ jQueryInputHint/js/inputHint.js
@@ -0,0 +1,21 @@
+// variable for storing text of label
+var labelText = $('label[for="q"]').text();
+$('#q').val( labelText);
+// adding hint class to input text
+$('#q').addClass('hint');
+// removing label 
+$('label[for="q"]').remove();
+// adding focus and blur events to input text
+$('#q').bind('focus blur ' , function(e) {
+  $(this).toggleClass('hint');
+  if(e.type == 'blur') {
+  	if($(this).val() == '') {
+      $(this).val(labelText);
+    }  
+  }
+  else {
+  	if($(this).val() == labelText) {
+	$(this).val('');
+    }
+  }
+});
