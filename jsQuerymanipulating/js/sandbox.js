f5fd5d35b7fe26c1f16901333770f43a676e1e1e
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-21 00:32:55 +0530

    jquery3

--- jsQuerymanipulating/js/sandbox.js
+++ jsQuerymanipulating/js/sandbox.js
@@ -7,12 +7,12 @@ for(var i = 0 ; i < 5 ; i++) {
 $('#myList li:odd').remove();
 
 // appending new h2 and paragraph in div.module2
-$('#specials').prepend('<h2>india is my country and i love india, we should respect our country and should held self for its destruction</h2>');
+$('div.module').last().prepend('<h2>Another Paragraph</h2><p>india is my country and i love india, we should respect our country and should held self for its destruction</p>');
 
 // adding wednesday in selection list
-$('select[name="day"]').append('<option>wednesday</option>');
+$('select[name="day"]').append('<option value="wednesday">Wednesday</option>');
 
 // adding new div and appending a image
 $('#specials').after('<div class = "module" id = "newDiv"></div>');
-$('#newDiv').append('<img src = "images/bread.jpg" alt= "bread" />');
-  
+var image = $('img:first').attr('src');
+$('div.module').last().append('<img src="' + image + '" />');
