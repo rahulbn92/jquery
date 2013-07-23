7dff8679d0d90a4d7863c102caddbebe9b4062e2
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-24 00:42:58 +0530

    jquery2

--- jQueryTraversing/js/sandbox.js
+++ jQueryTraversing/js/sandbox.js
@@ -6,18 +6,18 @@ $('img').each(function() {
 
 // adds a class searchclass to parent of input with name ="q"
 $('input[name="q"]').parent().addClass('searchClass')
-alert("class added to form with search input box is  " +$('#search').attr('class'))
+console.log("class added to form with search input box is  " +$('#search').attr('class'))
 
 // adds current class to sixth li and removes from the fifth li
 $('li.current').next().addClass('current')
-$("#myList li:nth-child(5)").removeClass()
-alert("after adding and removing class from li , the main list looks like  "  +$('#myList').html());
+$("li.current:first").removeClass()
+console.log("after adding and removing class from li , the main list looks like  "  +$('#myList').html());
 
 // traversing to go button from select with name ="day"
-alert("traversing from select to input button ");
-alert("input button value is " +$('select[name= "day"]').parent().next().children().val());
+console.log("traversing from select to input button ");
+console.log("input button value is " +$('#specials select').parents('#specials').find('input[type="submit"]').val());
 
 // adding class = current to first child  and class="disabled" to remaining children of ul with id= "myList" 
 $('#slideshow li:first-child').addClass('current')
 $('#slideshow li:first-child').nextAll().addClass('disabled')
-alert("after adding classes to li with parent id = myList , list looks like  " +$('#slideshow').html());
+console.log("after adding classes to li with parent id = myList , list looks like  " +$('#slideshow').html());
