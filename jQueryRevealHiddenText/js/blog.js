5e135a2d257d08951ddb108813a7ab55710a42a1
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-24 22:40:23 +0530

    jquery6

--- jQueryRevealHiddenText/js/blog.js
+++ jQueryRevealHiddenText/js/blog.js
@@ -1,7 +1,6 @@
-// setting the  href attribute to void(0)
-$('#blog a').attr('href','javascipt:void(0)');
 // binding the click function to slideup and slidedown
-$('#blog li h3').bind('click' , function() {
+$('#blog li h3').bind('click' , function(e) {
+  e.preventDefault();
   $(this).siblings('p').slideDown(1000);
   $(this).parent().siblings().children('p').slideUp(1000);
 });
