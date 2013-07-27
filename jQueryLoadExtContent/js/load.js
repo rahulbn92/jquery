c5e56d4d68abc08602f26a41cad36d96d77e7495
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-27 06:19:45 +0530

    load content

--- jQueryLoadExtContent/js/load.js
+++ jQueryLoadExtContent/js/load.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
   $('div.module h3').each(function(i) {
-  $(this).children('a').attr('href','javascript:void(0)');
-  // making new divs and adding classes to them
-  var $newDiv = $('<div class="' + (i + 1) + '"></div>').insertAfter($(this));
-  $(this).data('newDiv' , $newDiv);
+    var $newDiv = $('<div/>').insertAfter($(this));
+    $(this).data('newDiv' , $newDiv);
   });
   //adding click function to h3 elements
-  $('div.module h3').click(function() {
-    $(this).data('newDiv').load('blog.html #post' + $(this).data('newDiv').attr('class'));
+  $('div.module h3').click(function(e) {
+    e.preventDefault();
+    $(this).data('newDiv').load($(this).children('a').attr('href'));
   });
 });
 
