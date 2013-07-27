b814abf676ab1653269a1d71d7a02041c8ec3c4a
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-27 06:26:05 +0530

    load json content

--- jQueryJSONLoadContent/js/specials.js
+++ jQueryJSONLoadContent/js/specials.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 });
 
 function loadJSON () {
-  var $newDiv = $('<div></div>').insertAfter($('#specials form'));
+  var $newDiv = $('<div/>').insertAfter($('#specials form'));
   $('select[name="day"]').data('newDiv' ,$newDiv);
   var jsonObject 
   $.ajax({ url:"specials.json" , success:function(data) {
@@ -11,10 +11,10 @@ function loadJSON () {
   }});
   $('select[name="day"]').change(function(){
     var selectedDay = $('select[name="day"] option:selected').val();
-    var selectedKey =jsonObject[selectedDay];
+    var selectedKey = jsonObject[selectedDay];
     $(this).data('newDiv').css('color',  selectedKey.color);
-    $(this).data('newDiv').html(selectedKey.title + "<p>" + selectedKey.text + "</p>");
-    $(this).data('newDiv').append("<p><img src='images/special.jpg ' /></p>");
+                          .html(selectedKey.title + "<p>" + selectedKey.text + "</p>");
+                          .append("<p><img src='images/special.jpg ' /></p>");
   });
   $('.buttons').remove();
 }
