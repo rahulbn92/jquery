780b715130776817755c721c364645adf05767c2
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-31 22:54:05 +0530

    slideshow

--- jQuerySlideShow/js/slideshow.js
+++ jQuerySlideShow/js/slideshow.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
-  $('#slideshow').prependTo('body');
-  $('#slideshow').append('<nav id="displayCurrent"></nav>');
-});
-
-$('#slideshow li').hide();
-var i=0;
-slideShow();
-
-// function for slide showing images in div.module
-function slideShow() {  
-  $('#slideshow li:eq(' + i + ')').fadeIn(1000).delay(2000).fadeOut(1000);
-  $('#displayCurrent').html("there are total 3 images and you are watching " + $('#slideshow li:eq(' + i + ')').children("img").attr('alt') + " image");
-  i++;
-  if(i == 3) { 
-    i = 0; 
+$(document).ready(function(){
+  var totalImages = $('#slideshow').children("li").length;
+  var i = 0;
+  $('body').prepend("<div id='slideshowDiv' style='width:900px; height:390px'></div>")
+  $('#slideshowDiv').prepend($('#slideshow'));
+  $('#slideshow').children('li').hide()
+                           .parent().after("<div id='navigationDiv' class='current'></div>");
+  slideshow(totalImages, i);
+})
+//function to slideshow images
+function slideshow(length, i){
+  var image = $('#slideshow li:eq('+ (i++) +')');
+  $('#navigationDiv').html("(there are total '" + length + "' images: )" + "\n" + " And current image is '" + image.children("h2").text());
+  image.fadeIn(1000).delay(1000).fadeOut(1000);
+  if(i == length){
+    i = 0;
   }
-  setTimeout(slideShow,4000); 
+  setTimeout(function(){slideshow(length,i)},3000);
 }
 
 
