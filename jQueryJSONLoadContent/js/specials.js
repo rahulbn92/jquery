09a37e86251848c7c10959a5878e0c9fe76ef98b
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-07-31 23:15:40 +0530

    json

--- jQueryJSONLoadContent/js/specials.js
+++ jQueryJSONLoadContent/js/specials.js
@@ -4,17 +4,22 @@ $(document).ready(function(){
 
 function loadJSON () {
   var $newDiv = $('<div/>').insertAfter($('#specials form'));
-  $('select[name="day"]').data('newDiv' ,$newDiv);
+  $('select[name="day"]').data('newDiv', $newDiv);
   var jsonObject 
   $.ajax({ url:"specials.json" , success:function(data) {
     jsonObject = data;
   }});
-  $('select[name="day"]').change(function(){
+  $('select[name="day"]').change(function() {
     var selectedDay = $('select[name="day"] option:selected').val();
     var selectedKey = jsonObject[selectedDay];
-    $(this).data('newDiv').css('color',  selectedKey.color);
-                          .html(selectedKey.title + "<p>" + selectedKey.text + "</p>");
-                          .append("<p><img src='images/special.jpg ' /></p>");
+    if(selectedDay == 'Select') {
+      $(this).data('newDiv').html(" ");
+    }
+    else {
+      $(this).data('newDiv').css('color',  selectedKey.color)
+                            .html(selectedKey.title + "<p>" + selectedKey.text + "</p>")
+                            .append("<p><img src='images/special.jpg ' /></p>");
+    }
   });
   $('.buttons').remove();
 }
