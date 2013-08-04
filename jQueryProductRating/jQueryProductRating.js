c628620c3776ed948515a431b56386f5d3935a50
root <root@vinsol-HP-ProBook-4530s.(none)>
2013-08-04 06:23:32 +0530

    product ratinf

--- jQueryProductRating/jQueryProductRating.js
+++ jQueryProductRating/jQueryProductRating.js
@@ -7,7 +7,6 @@ function showProducts() {
   // function for choosing item
   $('body').delegate('.items', 'click' , function() {
     $item = $(this);
-    $clickedItem = $item.html();
     $('.items').removeClass('highlight');
     $item.addClass('highlight');
     $('.ratings').removeClass('highlight');
@@ -15,15 +14,11 @@ function showProducts() {
 
   // function for choosing specific rating type
   $('body').delegate('.ratings' , 'click' , function() { 
-    $rating = $(this);
-    $ratingValue = $rating.html();
+    var $rating = $(this);
     $('.ratings').removeClass('highlight');
     $rating.addClass('highlight');
-    $('input[type="radio"][name="' + $clickedItem + '"]').each(function() {
-      $(this).prop('checked', false);
-      $(this).prop('disabled', true);
-    }) 
-    $('input[type="radio"][name="' + $clickedItem + '"][value ="' + $ratingValue + '"]').prop('checked', true);
+    $('input[type="radio"][name="' + $item.attr('id') + '"]').prop('disabled', true);
+    $('input[type="radio"][name="' + $item.attr('id') + '"][value ="' + $rating.attr('id') + '"]').prop('checked', true);
     $('input[type="radio"]:checked').prop('disabled', false);
   });
 }
